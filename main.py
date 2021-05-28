9eef36cf8915353bb380418d70fd2a0483b47c5a
Jason-Cee <jasoncee017@gmail.com>
2021-05-28 14:05:40 +0200

    Updated

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # My email
 sender_email_id = 'jasoncee017@gmail.com'
 # Receiver email
-receiver_email_id = 'brentleejohnson73@gmail.com'
+receiver_email_id = ['brentleejohnson73@gmail.com', 'jaydenmay040@gmail.com', 'jasoncee017@gmail.com']
 # Password
-password = input('Enter senders email password: ')
+password = input('Please enter email password: ')
 # Security purposes
 s.starttls()
 # Message to be sent
 s.login(sender_email_id, password)
 # Message
-message = "Hi, How are you? I am find thanks\n"
-message = message + "How was your task yesterday?"
+message = "Hi\n"
+message = message + "Python email tester, Thank You"
 # Sending mail
 s.sendmail(sender_email_id, receiver_email_id, message)
 # Terminating session
